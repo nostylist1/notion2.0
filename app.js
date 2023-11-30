c6ce760eec5d4064fba8d50c11c6cc96d1f6febe
nostylist <152104813+nostylist1@users.noreply.github.com>
2023-11-30 03:01:17 +0300

    app.js

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
 const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
-// function render (){
-//     for(i = 0; i < notes.length; i++) {
-//         listElement.insertAdjacentHTML("beforeend",getNoteTemplate(notes[i]) ) 
-//     }
-// }
-
-// render()
-
-// createBtn.onclick = function() {
-//     if (inputElement.value.length === 0) {
-//         return 
-//     }
-//     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(inputElement.value)) 
-//     inputElement.value = ''
-// }
-
-// function getNoteTemplate(title) {
-//     return`             
-//         <li
-//             class="list-group-item d-flex justify-content-between align-items-center"
-//             >
-//             <span>${title}</span>
-//             <span>
-//             <span class="btn btn-small btn-success">&check;</span>
-//             <span class="btn btn-small btn-danger">&times;</span>
-//             </span>
-//         </li>`
-// }
-
-// const person = {
-//     firstName: 'Vlad',
-//     lastName: 'Krapko',
-//     nickName: 'nostylist',
-//     year: 20,
-//     getFullName: function() {
-//         console.log(person.firstName + ' ' + person.lastName)
-//     },
-// }
 
 const notes = [
     {
